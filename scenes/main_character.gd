c4d90e808b44f459b0d42be138356012c4a83e32
Hudson-Storti <hudsonstorti@gmail.com>
2024-10-16 20:41:26 -0300

    Pasta do Jogo Mago Esfomeado

---
+++ scenes/main_character.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -900.0
+@onready var sprite_2d = $Sprite2D
+
+func _physics_process(delta: float) -> void:
+	# Animation
+	if (velocity.x > 1 || velocity.y <-1):
+		sprite_2d.animation = "Run"
+	else:
+		sprite_2d.animation = "Walk"
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		sprite_2d.animation = "Jump"
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("Left", "Right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 15)
+
+	move_and_slide()
+	
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
