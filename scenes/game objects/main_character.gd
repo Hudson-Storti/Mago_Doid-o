e51f90e93aa7d7ed0fe80002bf9f0f29097f3c5c
Kkkaique <kaiquepotato@gmail.com>
2024-11-01 19:38:55 -0300

    Atualizações 01/11

--- scenes/game objects/main_character.gd
+++ scenes/game objects/main_character.gd
@@ -1,47 +1,61 @@
+# Player.gd
 extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -850.0
 @onready var sprite_2d = $Sprite2D
+@onready var life_container = get_node("/root/Node/UI/Hearts/HBoxContainer")  # Ajuste o caminho se necessário
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var is_jumping = false  # Controla se o personagem está no ar
+var is_jumping = false
+var lives = 3
+var checkpoint_position = Vector2()
 
 func jump():
 	velocity.y = JUMP_VELOCITY
-	is_jumping = true  # Marca que o personagem iniciou um pulo
+	is_jumping = true
+
+func jump_side(side_velocity: float):
+	velocity.y = JUMP_VELOCITY * 0.5
+	velocity.x = side_velocity
 
 func _physics_process(delta: float) -> void:
-	# Aplicando gravidade se não estiver no chão
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
-		# Ao tocar o chão, o personagem não está mais pulando
 		is_jumping = false
 
-	# Pular se a tecla Jump (Espaço ou W) for pressionada e o player estiver no chão
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		jump()
 
-	# Movimentação lateral com AWSD e Setas
 	var direction := Input.get_axis("ui_left", "ui_right")
-	print("Direction: ", direction)  # Adicione esta linha para debugar
 	if direction != 0:
 		velocity.x = direction * SPEED
-		sprite_2d.flip_h = direction < 0  # Troca direção do sprite
+		sprite_2d.flip_h = direction < 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, 15)  # Suaviza a parada
+		velocity.x = move_toward(velocity.x, 0, 15)
 
-	# Atualiza a animação
 	update_animation()
-
-	# Movimenta o personagem e lida com colisões
 	move_and_slide()
 
 func update_animation():
 	if is_jumping:
-		sprite_2d.animation = "Jump"  # Animação de pulo
+		sprite_2d.animation = "Jump"
 	elif abs(velocity.x) > 1:
-		sprite_2d.animation = "Run"  # Animação de correr
+		sprite_2d.animation = "Run"
 	else:
-		sprite_2d.animation = "Walk"  # Animação de andar/parado
+		sprite_2d.animation = "Walk"
+
+# Função para diminuir uma vida e atualizar a HUD
+func lose_life():
+	lives -= 1
+	life_container.update_lives(-1)  # Atualiza a HUD para refletir a perda de vida
+	if lives > 0:
+		position = checkpoint_position
+	else:
+		get_tree().change_scene("res://Menu.tscn")  # Volta para o menu ao perder todas as vidas
+		print("Game Over")
+
+func on_enemy_collision():
+	lose_life()
+	jump_side(-500 if position.x < checkpoint_position.x else 500)
